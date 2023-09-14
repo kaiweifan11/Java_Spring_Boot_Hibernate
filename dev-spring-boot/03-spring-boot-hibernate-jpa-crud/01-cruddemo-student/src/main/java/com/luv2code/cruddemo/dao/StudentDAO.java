1a3f8110d6b6d088b722713cd8679e443ba55692
kaiweifan11 <30461398+kaiweifan11@users.noreply.github.com>
2023-09-14 13:21:55 +0800

    Add files via upload

---
+++ dev-spring-boot/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,21 @@
+package com.luv2code.cruddemo.dao;
+
+import com.luv2code.cruddemo.entity.Student;
+import java.util.List;
+
+public interface StudentDAO {
+
+    void save(Student theStudent);
+
+    Student finaById(Integer Id);
+
+    List<Student> findAll();
+
+    List<Student> findByLastName(String theLastName);
+
+    void update(Student theStudent);
+
+    void delete(Integer Id);
+
+    int deleteAll();
+}
